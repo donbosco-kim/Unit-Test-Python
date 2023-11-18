a8d67b0480acc3cf69bc94d465911fb637fecb65
q742 <leonbosco2@gmail.com>
2023-11-17 21:21:03 -0600

    add test for start and end dates and tested

--- mod13_dk4d4.py
+++ mod13_dk4d4.py
@@ -1,5 +1,5 @@
 import unittest
-
+from datetime import datetime
 class TestUserInputs(unittest.TestCase):
     #symbol: capitalized, 1-7 alpha characters
     #test for valid symbols
@@ -43,6 +43,44 @@ class TestUserInputs(unittest.TestCase):
         for time_series in invalid_time_series:
             with self.subTest(time_series=time_series):
                 self.assertFalse(time_series.isnumeric() and 1 <= int(time_series) <= 4, f"Test failed for invalid time series: {time_series}")
+
+    #start date: date type YYYY-MM-DD
+    def test_valid_start_date(self):
+        valid_start_dates = ['2023-11-17']
+        for start_date in valid_start_dates:
+            with self.subTest(start_date=start_date):
+                self.assertTrue(start_date == datetime.strptime(start_date, "%Y-%m-%d").strftime("%Y-%m-%d"), f"Test failed for valid start date: {start_date}")
+
+    def test_invalid_start_date(self):
+        invalid_start_dates = ['', '17-11-2023', '-11-2023', '2023/11/17', 'A', 'a', '1']
+        for start_date in invalid_start_dates:
+            with self.subTest(start_date=start_date):
+                try:
+                    datetime.strptime(start_date, "%Y-%m-%d")
+                    is_valid = True
+                except ValueError:
+                    is_valid = False
+
+                self.assertFalse(is_valid, f"Test failed for invalid start date: {start_date}")
+
+    #end date: date type YYYY-MM-DD
+    def test_valid_end_date(self):
+        valid_end_dates = ['2023-11-17']
+        for end_date in valid_end_dates:
+            with self.subTest(end_date=end_date):
+                self.assertTrue(end_date == datetime.strptime(end_date, "%Y-%m-%d").strftime("%Y-%m-%d"), f"Test failed for valid start date: {end_date}")
+
+    def test_invalid_end_date(self):
+        invalid_end_dates = ['', '17-11-2023', '-11-2023', '2023/11/17', 'A', 'a', '1']
+        for end_date in invalid_end_dates:
+            with self.subTest(end_date=end_date):
+                try:
+                    datetime.strptime(end_date, "%Y-%m-%d")
+                    is_valid = True
+                except ValueError:
+                    is_valid = False
+
+                self.assertFalse(is_valid, f"Test failed for invalid start date: {end_date}")
 
 if __name__ == '__main__':
     unittest.main()
